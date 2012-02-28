2e1d2b291514dfc1ab8e3ceb67b87155db2c6bf4
Wei-Lun Hung <kinmener@gmail.com>
2012-02-28 00:01:52 -0600

    merge multi condition

--- src/monitor/AbstractImplicitMonitor.java
+++ src/monitor/AbstractImplicitMonitor.java
@@ -3,33 +3,41 @@ package monitor;
 
 //import java.util.ArrayList ;
 import java.util.concurrent.locks.Lock; 
-import java.util.concurrent.locks.Condition; 
 import java.util.concurrent.locks.ReentrantLock;
 
 public abstract class AbstractImplicitMonitor {
-  final Lock lock_ = new ReentrantLock(true);
-  volatile Thread occupant_ = null;
+    final Lock lock = new ReentrantLock(true);
+    volatile Thread occupant = null;
 
-  protected abstract void Enter(); 
+    protected abstract void Enter(); 
 
-  protected abstract void Leave(); 
+    protected abstract void Leave(); 
 
-  protected void DoWithin(Runnable runnable) {
-    Enter();
-    try {
-      runnable.run();  
-    } finally {
-      Leave();
+    public void DoWithin(Runnable runnable) {
+        Enter();
+        try {
+            runnable.run();  
+        } finally {
+            Leave();
+        }
     }
-  }
-  protected<T> T DoWithin( RunnableWithResult<T> runnable ) {
-    Enter() ;
-    try {
-      return runnable.run() ; }
-    finally {
-     /* if( occupant_ == Thread.currentThread() ) */ Leave() ; }
-  }
-  protected abstract ImplicitCondition makeCondition(Assertion assertion); 
+    public<T> T DoWithin( RunnableWithResult<T> runnable ) {
+        Enter() ;
+        try {   
+            return runnable.run() ; 
+        }
+        finally {
+            Leave() ; 
+        }
+    }
+    public<T extends Exception> void DoWithin( RunnableWithException<T> runnable ) throws T {
+      Enter() ;
+      try {
+        runnable.run() ; }
+      finally {
+       /* if( occupant_ == Thread.currentThread() ) */ Leave() ; }
+    }
+    public abstract AbstractCondition makeCondition(Assertion assertion); 
 }
 
 
--- src/monitor/NaiveImplicitMonitor.java
+++ src/monitor/NaiveImplicitMonitor.java
@@ -1,31 +1,22 @@
-
 package monitor;
 
-
-import java.util.concurrent.locks.Lock; 
 import java.util.concurrent.locks.Condition; 
-import java.util.concurrent.locks.ReentrantLock;
 
 public class NaiveImplicitMonitor extends AbstractImplicitMonitor {
-  private java.util.concurrent.locks.Condition condition_ = null;
-  //public NaiveImplicitMonitor(Assertion assertion) {}
-  protected final void Enter() {
-    lock_.lock();
-    occupant_ = Thread.currentThread();
-  }
-  protected final void Leave() {
-    if(condition_ != null) condition_.signal();
-    occupant_ = null;
-    lock_.unlock();
-  }
-  public void DoWithin( Runnable runnable ) {
-    super.DoWithin( runnable ) ; }
+    private Condition condition = null;
+    
+    protected final void Enter() {
+        lock.lock();
+        occupant = Thread.currentThread();
+    }
+    protected final void Leave() {
+        if(condition != null) condition.signal();
+        occupant = null;
+        lock.unlock();
+    }
 
-  public<T> T DoWithin( RunnableWithResult<T> runnable ) {
-    return super.DoWithin( runnable ) ; }
-
-  public ImplicitCondition makeCondition(Assertion assertion) {
-    if(condition_ == null) condition_ = lock_.newCondition();
-    return new ImplicitCondition(condition_, assertion);
-  }
+    public NavieCondition makeCondition(Assertion assertion) {
+        if(condition == null) condition = lock.newCondition();
+        return new NavieCondition(condition, assertion);
+    }
 }
